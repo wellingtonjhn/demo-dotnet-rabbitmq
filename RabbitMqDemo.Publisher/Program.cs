39f99279b19cb584e4fc2fd48acb54e915885ec0
Wellington Nascimento <wellington.jhn@gmail.com>
2018-02-10 04:02:00 -0200

    Add project files.

---
+++ RabbitMqDemo.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using EasyNetQ;
+using Newtonsoft.Json;
+using RabbitMqDemo.Messages;
+using System;
+using System.Threading.Tasks;
+
+namespace RabbitMqDemo.Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bus = RabbitHutch.CreateBus("amqp://hzlywamn:L0FaWRquJYSOa1yBiEcVfSX-WAuUdE4Z@crane.rmq.cloudamqp.com/hzlywamn");
+
+            for (int i = 0; i < 100; i++)
+            {
+                var name = Faker.NameFaker.FirstName();
+                var username = $"{name}_user_{i}";
+
+                var message = new UserCreated
+                {
+                    Username = username,
+                    Name = name,
+                    Email = Faker.InternetFaker.Email()
+                };
+
+                await bus.PublishAsync(message);
+
+                Console.WriteLine($"Message published: {JsonConvert.SerializeObject(message)}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMqDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using EasyNetQ;
+using Newtonsoft.Json;
+using RabbitMqDemo.Messages;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace RabbitMqDemo.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = RabbitHutch.CreateBus("amqp://hzlywamn:L0FaWRquJYSOa1yBiEcVfSX-WAuUdE4Z@crane.rmq.cloudamqp.com/hzlywamn");
+
+            bus.SubscribeAsync<UserCreated>(Assembly.GetExecutingAssembly().GetName().Name, userCreated =>
+            {
+                Console.WriteLine($"Message received: {JsonConvert.SerializeObject(userCreated)}");
+                return Task.CompletedTask;
+
+            }, configuration =>
+            {
+                configuration.WithDurable();
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
